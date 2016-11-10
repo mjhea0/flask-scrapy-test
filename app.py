5daac5141959cd32dba56d803d4080438009430c
Michael Herman <hermanmu@gmail.com>
2016-11-10 11:53:55 -0700

    added post endpoint

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ from twisted.web.server import Site
 from twisted.internet import reactor
 from arachne import Arachne
 
+from flask import request, jsonify
+
 app = Arachne(__name__)
+
+@app.route('/data', methods=['GET', 'POST'])
+def get_data():
+    if request.method == 'POST':
+        content = request.get_json(silent=True)
+        # grab URL from payload, pass to scrapy, fire scraper, return data
+        return jsonify(content)
+    return 'testing a GET request'
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
 site = Site(resource)
